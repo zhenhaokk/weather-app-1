039660d15b0bac1f29313b2935f53fab94220e37
zhenhaokk <zhenhao.d.pro@gmail.com>
2022-04-12 23:41:48 +1000

    a

---
+++ src/components/searchMain.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import "../components/style.css";
+import WeatherDetails from "./WeatherDetails";
+
+function SearchMain() {
+  const [searchTerm, setSearchTerm] = useState("Hobart");
+  const [countryN, searCountry] = useState('');
+  const [tempInfo, setTempInfo] = useState({});
+
+  const getWeatherInfo = async () => {
+    try {
+      let url = `http://api.openweathermap.org/data/2.5/weather?q=${searchTerm},${countryN}&units=metric&appid=1b2e0ad3de4bbd8aa7e989114e915dbb`;
+
+      let res = await fetch(url);
+      let data = await res.json();
+      const { temp, humidity, pressure } = data.main;
+      const { main: weatherType } = data.weather[0];
+      const { name } = data;
+      const { speed } = data.wind;
+      const { country, sunset } = data.sys;
+
+      const myNewWeatherInfo = {
+        temp,
+        humidity,
+        pressure,
+        weatherType,
+        name,
+        speed,
+        country,
+        sunset,
+      };
+
+      setTempInfo(myNewWeatherInfo);
+      // console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getWeatherInfo();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <div>
+      <div className="wrap">
+        <div className="search">
+          <input
+            type="search"
+            placeholder="Search city.."
+            id="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <input
+            type="search"
+            placeholder="country"
+            id="search"
+            value={countryN}
+            onChange={(e) => searCountry(e.target.value)}
+          />
+          <button className="searchButton" onClick={getWeatherInfo}>
+            Search
+          </button>
+        </div>
+      </div>
+      {/* This the the weather details page */}
+      <WeatherDetails {...tempInfo} />
+    </div>
+  );
+}
+
+export default SearchMain;
